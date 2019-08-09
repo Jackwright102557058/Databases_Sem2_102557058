51e4e7e63834f3fcccaae63f687817a93f70eab9
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-08-09 12:06:30 +1000

    TASK 11 COMMIT

--- week_2_tasks.sql
+++ week_2_tasks.sql
@@ -13,7 +13,7 @@ create TABLE player(
 
 create TABLE CLUB(
     Clubname NVARCHAR(100) PRIMARY KEY
-    , Cl_Contactname INTEGER Null
+    , Cl_Contactname NVARCHAR(100)
     
 );
 
@@ -24,13 +24,13 @@ create TABLE SEASON(
     ,PRIMARY KEY (Seasonname, Seasonyear)
 
     ,CONSTRAINT Check_Seasonname CHECK (Seasonname IN ('Winter','Summer'))
-)
+);
 
-create TABLE TEAMENTRY(
-    En_Teamnumber INTEGER
+create TABLE TEAMENTRY (
+    En_Teamnumber INT
     , En_Agegroup NVARCHAR(3)
     , Seasonname NVARCHAR(6)
-    , Seasonyear INTEGER
+    , Seasonyear INT
     , Clubname NVARCHAR(100)
     ,PRIMARY KEY (En_Teamnumber, En_Agegroup, Seasonname, Seasonyear, Clubname)
     ,FOREIGN KEY (Seasonname,Seasonyear) REFERENCES SEASON
@@ -41,32 +41,28 @@ create TABLE TEAMENTRY(
 create TABLE PLAYERREGISTRATION(
     Dateregistered DATE
     , Clubname NVARCHAR(100)
-    , Seasonyear INTEGER
-    , Seasonname VARCHAR(6)
+    , Seasonyear INT
+    , Seasonname NVARCHAR(6)
+    , En_Teamnumber INT
     , En_Agegroup NVARCHAR(3)
-    , En_Teamnumber INTEGER
-    , Playerid INTEGER
+    , Playerid INT
 
-    ,PRIMARY KEY (playerid,Clubname,Seasonname,Seasonyear,En_Agegroup,En_Teamnumber)
-    ,FOREIGN KEY (En_Agegroup,En_Teamnumber,Seasonname,Seasonyear,Clubname) REFERENCES TEAMENTRY
+    ,PRIMARY KEY (playerid,Clubname,Seasonname,Seasonyear,En_Teamnumber, En_Agegroup)
+    ,FOREIGN KEY (En_Teamnumber, En_Agegroup,Seasonname,Seasonyear,Clubname) REFERENCES TEAMENTRY
     ,FOREIGN KEY (Playerid) REFERENCES PLAYER
-)
+);
 
-DELETE FROM PLAYERREGISTRATION
-DELETE FROM TEAMENTRY
-DELETE FROM SEASON
-DELETE FROM CLUB
-DELETE FROM PLAYER
 
-INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10002,'John','Howard', 5552345)
-INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10003,'Julia','Gillard', 5553456)
+INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10002,'John','Howard', 5552345);
+INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10003,'Julia','Gillard', 5553456);
 
-INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('Mt Martha Basketball Club', 'Bob Jane')
+INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('Mt Martha Basketball Club', 'Bob Jane');
 
-INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Winter')
-INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Summer')
+INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Winter');
+INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Summer');
 
-INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (1,'U14','Summer',2018,'Mt Martha Basketball Club')
-INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (2,'U14','Summer',2018,'Mt Martha Basketball Club')
+INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (1,'U14','Summer',2018,'Mt Martha Basketball Club');
+INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (2,'U14','Summer',2018,'Mt Martha Basketball Club');
 
-INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10003,'Mt Martha Basketball Club',2018,'Summer','U14',2,01/01/2018)
+INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10003,'Mt Martha Basketball Club',2018,'Summer','U14', 2,01/01/2018)
+;
