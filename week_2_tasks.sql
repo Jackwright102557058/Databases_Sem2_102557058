441ab8355ba26189f8980fd0e4e594b295f85d49
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-08-09 12:39:06 +1000

    Task 14 Commit

--- week_2_tasks.sql
+++ week_2_tasks.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS PLAERREGISTRATION
-DROP TABLE IF EXISTS TEAMENTRY
-DROP TABLE IF EXISTS SEASON
-DROP TABLE IF EXISTS CLUB 
-DROP TABLE IF EXISTS PLAYER
+DROP TABLE IF EXISTS PLAYERREGISTRATION;
+DROP TABLE IF EXISTS TEAMENTRY;
+DROP TABLE IF EXISTS SEASON;
+DROP TABLE IF EXISTS CLUB;
+DROP TABLE IF EXISTS PLAYER;
 
 create TABLE player(
     playerid INTEGER PRIMARY Key 
@@ -52,17 +52,34 @@ create TABLE PLAYERREGISTRATION(
     ,FOREIGN KEY (Playerid) REFERENCES PLAYER
 );
 
+DELETE FROM PLAYERREGISTRATION;
+DELETE FROM TEAMENTRY;
+DELETE FROM SEASON;
+DELETE FROM CLUB;
+DELETE FROM PLAYER;
 
 INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10002,'John','Howard', 5552345);
 INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10003,'Julia','Gillard', 5553456);
+INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10004,'Sam','Sammer', 5553457);
+INSERT INTO PLAYER(Playerid,Pl_Fname,Pl_Lname,Pl_Phone) VALUES (10005,'Zac','Bob', 5553458);
 
 INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('Mt Martha Basketball Club', 'Bob Jane');
+INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('Pew Pew Shootem up Club', 'Beep Boop');
+INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('I Wanna Die Club', 'Moo Man');
+INSERT INTO CLUB(Clubname,Cl_Contactname) VALUES ('Please Dont Join Club', 'How To');
 
 INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Winter');
 INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2018, 'Summer');
+INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2017, 'Summer');
+INSERT INTO SEASON(Seasonyear, Seasonname) VALUES (2017, 'Winter');
 
 INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (1,'U14','Summer',2018,'Mt Martha Basketball Club');
 INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (2,'U14','Summer',2018,'Mt Martha Basketball Club');
+INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (4,'U14','Winter',2017,'I Wanna Die Club');
+INSERT INTO TEAMENTRY(En_Teamnumber,En_Agegroup,Seasonname,Seasonyear,Clubname) VALUES (3,'U14','Summer',2017,'Please Dont Join Club');
 
-INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10003,'Mt Martha Basketball Club',2018,'Summer','U14', 2,01/01/2018)
-;
+
+INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10002,'Mt Martha Basketball Club',2018,'Summer','U14', 2,'01/01/2018');
+INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10003,'Mt Martha Basketball Club',2018,'Summer','U14', 2,'01/01/2018');
+INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10004,'I Wanna Die Club',2017,'Winter','U14', 4,'08/02/2017');
+INSERT INTO PLAYERREGISTRATION(Playerid,Clubname,Seasonyear,Seasonname,En_Agegroup,En_Teamnumber,Dateregistered) VALUES (10005,'Please Dont Join Club',2017,'Summer','U14', 3,'03/02/2017');
